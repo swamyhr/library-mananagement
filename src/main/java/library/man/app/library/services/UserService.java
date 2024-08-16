9fe9e8b287cd2b6f76460f5eb413616bc95e168d
swamyhr <swamyrangannanayaka@gmail.com>
2024-08-16 20:38:31 +0530

    updated the UserController code

--- src/main/java/library/man/app/library/services/UserService.java
+++ src/main/java/library/man/app/library/services/UserService.java
@@ -1,4 +1,15 @@
 package library.man.app.library.services;
 
-public class UserService {
+import library.man.app.library.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    public User createUser(User user);
+
+    public List<User> getUsers();
+
+    public Optional<User> getUserById(int id);
 }
--- src/main/java/library/man/app/library/servicesImpl/UserServiceImpl.java
+++ src/main/java/library/man/app/library/servicesImpl/UserServiceImpl.java
@@ -1,4 +1,32 @@
 package library.man.app.library.servicesImpl;
 
-public class UserServiceImpl {
+import library.man.app.library.models.User;
+import library.man.app.library.repositories.UserRepository;
+import library.man.app.library.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    public UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+         return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
 }
