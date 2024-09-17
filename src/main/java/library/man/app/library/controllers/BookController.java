414184b4e9b6d4e5b4e29c3ee39196cf604ea579
swamyhr <swamyrangannanayaka@gmail.com>
2024-09-17 19:12:59 +0530

    added implementation for book controller, service, repository

--- src/main/java/library/man/app/library/controllers/BookController.java
+++ src/main/java/library/man/app/library/controllers/BookController.java
@@ -1,4 +1,32 @@
 package library.man.app.library.controllers;
 
+import library.man.app.library.models.Book;
+import library.man.app.library.servicesImpl.BookServiceImpl;
+import library.man.app.library.utils.ResponseObj;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/books")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
+
+    @Autowired
+    public BookServiceImpl bookService;
+
+    @GetMapping("")
+    public List<Book> getBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping("")
+    public ResponseObj saveNewBook(@RequestBody Book book) {
+        bookService.createBook(book);
+        return new ResponseObj(201, "New Book Saved successfully");
+    }
 }
--- src/main/java/library/man/app/library/services/BookService.java
+++ src/main/java/library/man/app/library/services/BookService.java
@@ -1,4 +1,15 @@
 package library.man.app.library.services;
 
+import library.man.app.library.models.Book;
+import java.util.List;
+import java.util.Optional;
+
 public interface BookService {
+
+    public List<Book> getAllBooks();
+
+    public Optional<Book> getBookById(int bookId);
+
+    public Book createBook(Book newBook);
+
 }
